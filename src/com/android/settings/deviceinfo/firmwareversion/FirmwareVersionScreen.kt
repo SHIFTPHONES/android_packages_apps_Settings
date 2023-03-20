af95a16ffd0cb33f1e827c7521c88742f438f8c4
Alexander Martinz <amartinz@shiftphones.com>
2023-03-20 19:45:05 +0100

    (SHIFT) deviceinfo: add build id preference
    This adds the build id to settings to let users verify
    the base AOSP tag we are based on.
    
    The entry can be found at:
      - Settings -> About -> Android version -> Build id
    
    Change-Id: I29eb961c6b63d5381859aeb34cdc658d75dbe201
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
@@ -77,6 +77,7 @@ open class FirmwareVersionScreen : PreferenceScreenMixin, PreferenceSummaryProvi
             +MainlineModuleVersionPreference()
             +BasebandVersionPreference()
             +KernelVersionPreference()
+            +ShiftOsBuildIdPreference()
             +LineageBuildDatePreference()
             +SimpleBuildNumberPreference()
         }
