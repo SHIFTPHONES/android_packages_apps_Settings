21cf993ca63679334118b5bbcf4ef7eb86f3fe76
Alexander Martinz <amartinz@shiftphones.com>
2024-09-20 10:42:00 +0200

    (SHIFT) deviceinfo: add build fingerprint preference entry
    Change-Id: Ibefc5a6e914557d8ab3e7307669b91bb5da16b70
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

---
+++ src/com/android/settings/deviceinfo/firmwareversion/BuildFingerprintPreference.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) SHIFT GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion
+
+import android.content.Context
+import android.os.Build;
+import android.text.BidiFormatter;
+import androidx.preference.Preference
+import com.android.settings.R
+import com.android.settings.contract.TAG_DEVICE_STATE_PREFERENCE
+import com.android.settingslib.metadata.PreferenceMetadata
+import com.android.settingslib.metadata.PreferenceSummaryProvider
+import com.android.settingslib.preference.PreferenceBinding
+
+class BuildFingerprintPreference :
+    PreferenceMetadata, PreferenceSummaryProvider, PreferenceBinding {
+
+    override val key: String
+        get() = "build_fingerprint"
+
+    override val title: Int
+        get() = R.string.build_fingerprint
+
+    override val indexable
+        get() = false
+
+    override fun tags(context: Context) = arrayOf(TAG_DEVICE_STATE_PREFERENCE)
+
+    override fun bind(preference: Preference, metadata: PreferenceMetadata) {
+        super.bind(preference, metadata)
+        preference.isCopyingEnabled = true
+    }
+
+    override fun getSummary(context: Context): CharSequence =
+        BidiFormatter.getInstance().unicodeWrap(Build.FINGERPRINT);
+}
